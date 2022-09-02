6caa461a1ab9491085e33bd869be3bb2bf750198
Shahriar-H <67462321+Shahriar-H@users.noreply.github.com>
2022-09-02 18:05:15 +0600

    Add files via upload

---
+++ db.js
@@ -0,0 +1,12 @@
+const tags= require("./data/tags");
+const videos = require("./data/videos");
+const videos2 = require("./data/videos2");
+const transactions = require("./data/transactions");
+
+module.exports = {
+  tags,
+  videos,
+  videos2,
+  transactions,
+}
+
